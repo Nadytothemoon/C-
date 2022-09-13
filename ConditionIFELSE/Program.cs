321c230feb51695dfaf629648fe51aa650c1a344
Nady <nadyyakunina@gmail.com>
2022-09-13 21:33:17 +0400

    IFElse

---
+++ ConditionIFELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя:");
+string username = Console. ReadLine();
+
+if (username. ToLower() == "nady")
+{
+    Console.WriteLine ("Ура, это же Nady!!!");
+}
+else
+{
+    Console.Write ("Hello,");
+    Console.WriteLine (username);
+}
