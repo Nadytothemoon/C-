5d3a7236ee6dac2deea5091b169cc773aa72d152
Nady <nadyyakunina@gmail.com>
2022-09-13 19:12:29 +0400

    Hello User

--- Hello user/Program.cs
+++ Hello user/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello8, World!");
+﻿Console.Write ("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write ("Привет");
+Console.Write (username);
+
